49622ed0f4c5ebc772748496ccb020336b9b902f
Joshua Robinson <joshua.fe.robinson@gmail.com>
2018-11-19 09:47:02 +0000

    Restructured repository so root no longer contains module, meaning we can import the installed module from within the root directory.

--- setup.py
+++ setup.py
@@ -2,10 +2,11 @@
 
 from setuptools import setup, Extension, find_packages
 
-cpp_args = ['-std=c++14', '-mtune=native', '-march=native', '-Ofast']
+cpp_args = ['-std=c++14', '-mtune=native', '-march=native', '-Ofast', '-Wall', '-Wextra']
+link_args =  ['-Wl,--unresolved-symbols=report-all']
 
 ext_modules = [
-    Extension('mdma/spatial/distance', ['src/spatial/distance.cc'],
+    Extension('mdma/spatial/distance', ['src/pybind11/spatial/distance.cc'],
               include_dirs=['pybind11/include', '/usr/include/eigen3'],
               language='c++',
               extra_compile_args = cpp_args,
@@ -21,7 +22,8 @@ setup(
     url='https://github.com/tranqui/MDMAmazing.git',
     license='GNU General Public License v3.0',
     version='0.1.dev0',
-    packages=find_packages(),
+    package_dir={'': 'src'},
+    packages=find_packages('src'),
     #ext_modules=ext_modules,
     install_requires=['pybind11', 'numpy', 'scipy', 'pandas', 'mpi4py', 'natsort', 'progressbar', 'lxml', 'beautifulsoup4']
  )
